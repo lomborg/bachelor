8faa16ca8df3cb872d111d52ef37e8c9674f5a1b
Unknown <d.lomborg@hotmail.com>
2018-05-11 15:43:59 +0200

    Vinkla installed

--- config/app.php
+++ config/app.php
@@ -209,6 +209,8 @@ return [
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
         'Socialite' => Laravel\Socialite\Facades\Socialite::class,
+        'Facebook' => Vinkla\Facebook\Facades\Facebook::class,
+
 
     ],
 
